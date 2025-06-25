6bcc430802c0aea9410c62135bfb2522eeee5171
amanmishra75way <amishra.75way@gmail.com>
2025-06-25 18:51:38 +0530

    implementation of form vaildations

--- src/App.jsx
+++ src/App.jsx
@@ -8,17 +8,25 @@ const App = () => {
     watch,
     formState: { errors },
   } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
     <div>
-      <form onSubmit={handleSubmit((data) => console.log(data))}>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div>
           <label>First Name</label>
-          <input type="text" {...register("firstName")} />
+          <input type="text" {...register("firstName", { required: true, minLength: 1 })} />
+          {errors.firstName && <p>First name is required</p>}
         </div>
+
         <div>
           <label>Last Name</label>
           <input type="text" {...register("lastName")} />
         </div>
+
         <button type="submit">Submit Now</button>
       </form>
     </div>
