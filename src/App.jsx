171522f3aa89b28b216ddbd2eaa4898448c7eec3
amanmishra75way <amishra.75way@gmail.com>
2025-06-25 18:33:27 +0530

    Basic  way form creation state handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 const App = () => {
-  return <div>Thsis is apap</div>;
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  return (
+    <div>
+      <form onSubmit={handleSubmit((data) => console.log(data))}>
+        <div>
+          <label>First Name</label>
+          <input type="text" {...register("firstName")} />
+        </div>
+        <div>
+          <label>Last Name</label>
+          <input type="text" {...register("lastName")} />
+        </div>
+        <button type="submit">Submit Now</button>
+      </form>
+    </div>
+  );
 };
 
 export default App;
